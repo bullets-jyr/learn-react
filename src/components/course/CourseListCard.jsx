4a1f5f45980cd52b1dd387f56987440331878c17
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-29 13:01:01 +0900

    컴포넌트에 props 전달하기

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,17 +1,24 @@
+import Card from '../Card';
 import CourseItem from './CourseItem'
 
-function CourseListCard(props) {
+function CourseListCard({ items }) {
+
+    const [course1, course2, course3] = items;
+
     return (
-        <div className="card"  style={{ backgroundColor: 'black', color: 'white' }}>
-            <div className="card__header">강의 목록</div>
-            <div className="card__body">
-                <div className="courses">
-                    <CourseItem />
-                    <CourseItem />
-                    <CourseItem />
-                </div>
+        <Card title='강의 목록'>
+            <div className='courses'>
+                <CourseItem
+                    {...course1}
+                />
+                <CourseItem
+                    {...course2}
+                />
+                <CourseItem
+                    {...course3}
+                />
             </div>
-        </div>
+        </Card>
     );
 }
 
