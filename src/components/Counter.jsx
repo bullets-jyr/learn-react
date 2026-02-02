6faa5558e18705355c30f4be0b8e4f7f45841bdc
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-02 11:50:22 +0900

    스냅샷 처럼 동작하는 State

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,19 @@
-// function Counter() {
-//     return (
-//         <button>Counter</button>
-//     )
-// }
-//
-// export default Counter;
 import { useState } from "react"
 
 export default function Counter({ onTotal }) {
     const [counter, setCounter] = useState(0);
 
+    console.log('[렌더링] Counter: ', counter);
+
     const handleCounter = () => {
-      setCounter(counter + 1);
-      onTotal();
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+        console.log('[함수호출] Counter: ', counter);
+
+        if (onTotal) {
+            onTotal();
+        }
     }
     return (
         <button onClick={handleCounter}>Counter: {counter}</button>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,26 @@
-// import Counter from "./Counter";
-//
-// function Main() {
-//     return (
-//         <main>
-//             <Counter />
-//         </main>
-//     )
-// }
-//
-// export default Main;
-
 import { useState } from "react";
 import Counter from "./Counter";
 
 export default function Main() {
+
     const [total, setTotal] = useState(0);
+    const [flag, setFlag] = useState(false);
 
     const handleTotal = () => {
-      setTotal(total + 1);
+        setTotal(total + 1);
     }
 
     return (
         <main>
             <h2>total: {total}</h2>
+            <h2>flag: {flag.toString()}</h2>
+            <button onClick={() => setFlag(!flag)}>toggle flag</button>
+            <hr />
             <Counter onTotal={handleTotal} />
-            <br />
-            <br />
+            <hr />
             <Counter onTotal={handleTotal} />
+            <hr />
+            <Counter />
         </main>
     )
 }
